88266d79e46ae980175b8c58e41b3d9d51b7f1c7
System Administrator <root@YanLuMBP.(none)>
2013-06-05 00:01:08 +0100

    all bug in registration solved , registration works perfictly with all error displayed nicely

--- src/PHP/register.php
+++ src/PHP/register.php
@@ -2,35 +2,19 @@
 
 
 <?php
-
 	//load debugging lib
 	require_once('../Library/PhpConsole.php');
 	PhpConsole::start(true, true, dirname(__FILE__));
 
-	//load files
-	//require_once('db.php');
-	//require_once('user.php');
-	//establis a connection to DB
-	// if(!$GLOBALS['DB'] = pg_connect("host="%s" port=%d dbname="%s" user="%s" password="%s",DB_HOST,DB_PORT,DB_NAME,DB_USER,DB_PASSWORD")){
-	// die('Error:Unable to connect the database');}
-	// else echo "success";
 	if(!$GLOBALS['DB'] = pg_connect("host=localhost port=5432 dbname=webapp2013 user=postgres password=luningyuan7210")){
 	die('Error:Unable to connect the database </br>');
-	}else echo "success to connect the database </br>";
+	}
+	//else echo "success to connect the database </br>";
 
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	require_once('user.php');
-	//if(User::validateUsername($username)) echo "valid" else echo "invalid";
-// 	if(User::validateUsername($username)){
-// 		if ($password == null) echo "password null";
-// 		$user = new User();
-// 		$user->__set("username",$username);
-// 		$user->__set("password",$password);
-// 		$user->__get("username");
-// 	}else echo "not validate username</br>";
 	$user = new User();
 	$user = $user->__set_username_password_to_DB($username,$password);
-// 	$user->__set("password",$password);
-	printf("username = %s",$user->__get("username") );
+
 ?>
--- src/PHP/user.php
+++ src/PHP/user.php
@@ -10,12 +10,11 @@ class User {
 	public function __construct(){
 		$this->uid ='';
 		$this->fields = array("username" =>'',"password"=>'');
-		echo "construct success</br>";
 	}
 
 	//get function
 	public function __get($field) {
-		if($field == 'userId') {
+		if($field == "user_id") {
 			return $this->uid;
 		}else {
 			return $this->fields[$field];
@@ -26,11 +25,16 @@ class User {
 	public function __set($field,$value){
 		if(array_key_exists($field, $this->fields)){
 			$this->fields[$field] =$value;
-			printf("value=%s",$this->__get($field));
+			//printf("value=%s",$this->__get($field));
 		} else echo "field not exist in __set </br>";
 	}
 
 	public function __set_username_password_to_DB($username,$password){
+		if (User::get_by_name($username)) {
+			echo "user name exist";
+			return false;
+		}
+		
 		if ($password != null ){
 			if(User::validateUsername($username)){
 				$user = new User();
@@ -40,82 +44,91 @@ class User {
 							pg_escape_string($user->username),
 							pg_escape_string($user->password)
 					);
-					if(pg_query($GLOBALS['DB'],$query)){
+				if(pg_query($GLOBALS['DB'],$query)){
 						//successfully inserted
-						echo $user->username;
+						echo "register successful</br>";
 						return $user;
 					} else
+						echo "error to save user name and password to database</br>";
 						return false;
-			}else echo "error:not a valide username </br>";
-		} else echo "error:password needed </br>";
+			}else {
+				echo "error:not a valide username </br>"; //using ajax
+				return false;
+			}
+		} else {
+			echo "error:password needed </br>"; //using ajax -> javascript
+			return false;
+		}
 	}
 
 	//check validity functions
 	public static function validateUsername($username){
 		return preg_match('/^[A-Z0-9]{2,20}$/i',$username);
 	}
-
+	
 
 	//return an obj populated based on a user_id
-	public static function getById($user_id){
-		$user = new User();
-		$query = sprintf('SELECT USERNAME,PASSWORD,FROM %sUSER where USER_ID = $d',DB_TABLE_PREFIX,$user_id);
-		$result=pg_query($query,$GLOBALS['DB']);
-		if(pg_num_rows($result)){   		//check if query exist
-			$row = pg_fetch_assoc($result); //fetch contends
-			$user->username=$row['USERNAME'];
-			$user->password=$row['PASSWORD'];
-			$user->uid = $user_id;
-		}
-		pg_free_result($result);  //free result memory
-		return $user;
-	}
+// 	public static function getById($user_id){
+// 		$user = new User();
+// 		$query = sprintf('SELECT USERNAME,PASSWORD,FROM %sUSER where USER_ID = $d',DB_TABLE_PREFIX,$user_id);
+// 		$result=pg_query($query,$GLOBALS['DB']);
+// 		if(pg_num_rows($result)){   		//check if query exist
+// 			$row = pg_fetch_assoc($result); //fetch contends
+// 			$user->username=$row['USERNAME'];
+// 			$user->password=$row['PASSWORD'];
+// 			$user->uid = $user_id;
+// 		}
+// 		pg_free_result($result);  //free result memory
+// 		return $user;
+// 	}
 
-	//return an obj populated based on a username
-	public static function getByName($user_name){
+	//return an obj populated based on a username,if not found obj,then return false
+	public static function get_by_name($username){
 		$user = new User();
-		$query = sprintf('SELECT USER_ID,PASSWORD,EMAIL_ADDR,IS_ACTIVE FROM %sUSER where USER_NAME = $d',DB_TABLE_PREFIX,$user_name);
-		$result=pg_query($query,$GLOBALS['DB']);
-		if(pg_num_rows($result)){   		//check if query exist
+		$query = sprintf('SELECT user_id,password FROM table_register WHERE username = \'%s\'',pg_escape_string($username));
+		$result=pg_query($GLOBALS['DB'],$query);
+		if(pg_num_rows($result)!=0){//check if query exist
 			$row = pg_fetch_assoc($result); //fetch contends
-			$user->username=$user_name;
-			$user->password=$row['PASSWORD'];
-			$user->uid = $user_id;
+			$user->username=$username;
+			$user->password=$row['password'];
+			$user->uid = $row['user_id'];
+		}else {
+			return false;
 		}
-		pg_free_result($result);  //free result memory
+ //free result memory
 		return $user;
 	}
 
 	//save the record to the DB
-	public function save(){
-		$escaped_username= pg_escape_string($this->username);
-		if($this->username == null) echo "fucking error!";
+// 	public function save(){
+// 		$escaped_username= pg_escape_string($this->username);
+// 		if($this->username == null) echo "fucking error!";
 
-		if($this->uid != null)
-		{		//if uid exist then update
-			$query=sprintf('UPDATE table_register SET username = \'%s\', password =\'%s\' WHERE user_id = %d',
-					Table_REGISTER,
-					pg_escape_string( $GLOBALS['DB'],$this->username),
-					pg_escape_string($GLOBALS['DB'],$this->password),
-					$this->uid);
-			return pg_query($query,$GLOBALS['DB']);
-		}
-		else
-		{				//if user doesn't exist(not registered yet) then insert
-			$query=sprintf
-			('INSERT INTO table_register (username,password) VALUES (\'%s\',\'%s\')',
-					pg_escape_string($this->username),
-					pg_escape_string($this->password)
+// 		if($this->uid != null)
+// 		{		//if uid exist then update
+// 			$query=sprintf('UPDATE table_register SET username = \'%s\', password =\'%s\' WHERE user_id = %d',
+// 					Table_REGISTER,
+// 					pg_escape_string( $GLOBALS['DB'],$this->username),
+// 					pg_escape_string($GLOBALS['DB'],$this->password),
+// 					$this->uid);
+// 			return pg_query($query,$GLOBALS['DB']);
+// 		}
+// 		else
+// 		{				//if user doesn't exist(not registered yet) then insert
+// 			$query=sprintf
+// 			('INSERT INTO table_register (username,password) VALUES (\'%s\',\'%s\')',
+// 					pg_escape_string($this->username),
+// 					pg_escape_string($this->password)
 
-			);
-			if(pg_query($GLOBALS['DB'],$query)){
-				//successfully inserted
-				echo $this->username;
-				return true;
-			} else
-				return false;
-		}
-	}
+// 			);
+// 			if(pg_query($GLOBALS['DB'],$query)){
+// 				//successfully inserted
+// 				echo $this->username;
+// 				return true;
+// 			} else
+// 				return false;
+// 		}
+// 	}
 }
 
 ?>
