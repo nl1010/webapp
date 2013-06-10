763a41ca4e067e3a38dcec373e81f33d28312baa
System Administrator <root@YanLuMBP.(none)>
2013-06-10 21:55:57 +0100

    testing ajax

--- src/PHP/login.php
+++ src/PHP/login.php
@@ -14,18 +14,5 @@
 	require_once('user.php');
 	$user_id = User::password_check($username,$password);
 
-// 	
-// 	$user = User::get_by_name($username);
-// 	if($user == false){
-// 		echo "user doesn't exist";
-// 	}else {
-// 		if($password == null) {
-// 			echo "password cannot be empty";
-// 		} else {
-// 			if($user->__get("password")!=$password){
-// 				echo "incorrect password please retype again";
-// 			}
-// 		}
-// 	}
-	
+
 ?>
