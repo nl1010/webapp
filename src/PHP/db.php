69dbd3fa3c67b73e000ad11e515970590f535ea5
System Administrator <root@YanLuMBP.(none)>
2013-06-13 05:29:28 +0100

    start testing message

--- src/PHP/db.php
+++ src/PHP/db.php
@@ -1,16 +1,7 @@
 <?php
 
-//DB connection&schema constants
-define('DB_HOST','localhost');
-define('DB_PORT','5432');
-define('DB_USER','postgres');
-define('DB_PASSWORD','luningyuan7210');
-define('DB_NAME','webapp2013');
-define('Table_REGISTER','table_register');
-//establis a connection to DB
-if(!$GLOBALS['DB'] = pg_connect("host=%s port=%d dbname=%s user=%s password=%s",DB_HOST,DB_PORT,DB_NAME,DB_USER,DB_PASSWORD)){
-	die('Error:Unable to connect the database');
+if(!$GLOBALS['DB'] = pg_connect("host=localhost port=5432 dbname=webapp2013 user=postgres password=luningyuan7210")){
+	die('Error:Unable to connect the database </br>');
 }
-
 
 ?>
---
+++ src/PHP/message.php
@@ -0,0 +1,17 @@
+<?php 
+require 'db.php';
+
+Class Message{
+	private $x;
+	private $y;
+	private $message = 
+	
+	public function __construct(){
+		$this->x = null;
+		$this->y = null;
+		
+	}
+}
+
+
+?>
