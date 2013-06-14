a73ca90af74869a09f54a42d2a102e1690b8e935
System Administrator <root@YanLuMBP.(none)>
2013-06-14 16:28:08 +0100

    testing message

--- reference_doc_of_yan/ajaxTest/name.php
+++ reference_doc_of_yan/ajaxTest/name.php
@@ -1,4 +1,16 @@
 
 <?php
-echo "hello";
+
+if(isset($_POST['name']) ===true && empty ($_POST['name']) === false ){
+	require 'connect.php';
+	
+	$query = pg_query("
+			SELECT 'names'.'location'
+			FROM 'names'
+			WHERE 'names'.'name'=' ".pg_real
+			
+			")
+}
+
+
 ?>
--- src/PHP/message.php
+++ src/PHP/message.php
@@ -1,16 +1,49 @@
-<?php 
-require 'db.php';
-
+<?php
+#---class Message------
 Class Message{
 	private $x;
 	private $y;
-	private $message = 
+	private $message;
 	
 	public function __construct(){
 		$this->x = null;
 		$this->y = null;
-		
+		$this->message = null;
 	}
+	
+	public function __get($field){
+		if ($field == "x") {
+			return $this->x;
+		}else if($field == "y"){
+			return $this->y;
+		}else if($field == "message"){
+			return $this->message;
+		}else echo "there is no such field";
+	}
+	
+	public function __set($field,$value){
+		if($field == "x"){
+			$this->x = $value;
+		}else if ($field = "y"){
+			$this->y = $value;
+		}else if ($field == "message"){
+			$this->message=$value;
+		}
+	}
+	
+	public function __get_message($x,$y){
+		$query = sprintf('
+				SELECT message FROM message_table WHERE x=\'%x\' y=\'%y\' 
+				');
+		$result = pg_query($GLOBALS['DB'],$query);
+		if(pg_num_rows($result)!=0){
+			$row = pg_fetch_assoc($result);
+			$message = $row['message'];
+			return $message;
+		}else return false ; #message doesn't exist
+	}
+	
+	
 }
 
 
